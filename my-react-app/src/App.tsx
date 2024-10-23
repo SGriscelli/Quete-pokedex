c513613889fb9313701762dcff91e39077e7391d
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-10-23 10:38:47 +0200

    Mew devrait être correct maintenant

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+    imgSrc: "", // Missing image
+  },
+];
+
 function App() {
   return (
     <section>
-      <PokemonCard />
+      {pokemonList.map((pokemon, index) => (
+        <PokemonCard key={index} pokemon={pokemon} />
+      ))}
     </section>
   );
 }
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,27 +1,14 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
-
-    return (
-        <>
-            <figure>
-                <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>
-                <figcaption>{pokemon.name}</figcaption> 
-            </figure>
-        </>
-     
-    );
-    
-}
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <div className="pokemon-card">
+      <h2>{pokemon?.name || "Unknown Pokémon"}</h2>
+      {pokemon?.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+    </div>
+  );
+};
 
 export default PokemonCard;
