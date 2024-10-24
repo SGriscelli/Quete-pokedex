969578adfde1de4f154f641a4300bbedef881771
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-10-24 08:59:55 +0200

    quete6

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,26 +1,62 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard.tsx";
+import { useState } from "react";
 
-
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 
 function App() {
-  const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const pokeSuivant = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+  const pokePrecedent = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
   return (
     <section>
-      <div>
-      <PokemonCard pokemon={pokemonList[1]} />
-      </div> 
+      
+     
+      
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
+      {/* Boutons pour naviguer entre les Pokémon */}
+      <button type="button" onClick={pokePrecedent} disabled={pokemonIndex === 0}>
+        Précédent
+      </button>
+      <button type="button" onClick={pokeSuivant} disabled={pokemonIndex === pokemonList.length - 1}>
+        Suivant
+      </button>
     </section>
   );
 }
+
 export default App;
